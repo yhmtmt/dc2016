245d52ff93038081d3ca583003da707ad3381359
Yohei Matsumoto <matumoto@kaiyodai.ac.jp>
2016-11-29 15:54:53 +0900

    Update. ECEF conversion is added.

--- dataloader.cpp
+++ dataloader.cpp
@@ -13,10 +13,38 @@ struct aisdata{
 	float sog;			// 9. speed over ground
 	double lat;			// 11. latitude
 	double lon;			// 12. longitude
+	float x, y, z;		// ecef coordinate
 	float cog;			// 13. course over ground 
 	short hdg;			// 14. heading
 };
 
+#define PI 3.1415926535898			
+#define AE 6378137.					// radius at the equator  
+#define FE 1/298.257223563			// earth flatness
+#define BE (AE*(1.-FE))
+#define EE2 (2.*FE-FE*FE) 
+#define EE2_A ((AE*AE-BE*BE)/(AE))
+#define EE2_B ((AE*AE-BE*BE)/(BE))
+#define RE 6371229.3				// radius of the earth
+#define TERREF 0.0784				// terrestrial refraction coefficient
+#define TERREF2 2.07
+#define KNOT 0.514444444			/* in meter/sec*/
+#define MILE 1852					/* in meter */
+
+void bihtoecef(const float lat, const float lon, const float alt, 
+			   float & x, float & y, float & z)
+{
+	double slat = sin(lat);
+	double clat = cos(lat);
+	double slon = sin(lon);
+	double clon = cos(lon);
+	double N = AE / sqrt(1 - EE2 * slat * slat);
+
+	double tmp = (N + alt) * clat;
+	x = (float)(tmp * clon);
+	y = (float)(tmp * slon);
+	z = (float)((N * (1 - EE2) + alt) * slat);
+}
 
 int main(int argc, char ** argv)
 {
@@ -77,12 +105,17 @@ int main(int argc, char ** argv)
 		data.cog = (float) atof(token[13]);
 		data.hdg = (short) atoi(token[14]);
 		
+		bihtoecef(
+			(float)(data.lat * (PI / 180.)), 
+			(float)(data.lon * (PI / 180.)),
+			0.0, data.x, data.y, data.z);
 		// Printing out
 		cout << "MMSI: " << data.mmsi 
 			<< " Date: " << data.year << "-" << data.mon << "-" << data.day << "-" << data.t
 			<< " ROT: " << data.rot << " SOG: " 
 			<< data.sog << " LAT: " << data.lat << " LON: " << data.lon 
-			<< " COG: " << data.cog << " HDG: " << data.hdg << endl;			
+			<< " COG: " << data.cog << " HDG: " << data.hdg 
+			<< " ECEF: ("<< data.x << "," << data.y << "," << data.z << ")" << endl;						
 	}
 	return 0;
 }
